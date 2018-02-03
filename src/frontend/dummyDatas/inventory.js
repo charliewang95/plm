e1e887f974764c1ca7a9e4ac8646b87e75bf2da2
Dan Sun <ds318@duke.edu>
2018-02-03 17:07:49 -0500

    finished axios for inventory

---
+++ src/frontend/dummyDatas/inventory.js
@@ -0,0 +1,18 @@
+//inventory.js
+const sampleInventory = {
+	userId: '5a728166463929136ea011b5',
+	ingredientId: '5a73d553d771541fc1e621e2',
+	ingredientName: 'potato',
+	temperatureZone: 'warehouse',
+    quantity: 20 // in pounds
+	}
+
+const updatedInventory = {
+	userId: '5a728166463929136ea011b5',
+	ingredientId: '5a73d553d771541fc1e621e2',
+	ingredientName: 'potato',
+	temperatureZone: 'warehouse',
+    quantity: 10 // in pounds
+	}
+
+export {sampleInventory, updatedInventory}
