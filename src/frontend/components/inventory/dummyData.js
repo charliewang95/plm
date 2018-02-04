205e0253cf5b684a3ccd284d9f805ce5504c5981
pratiksha sharma <pratikshasharma@pratikshas-MacBook-Pro.local>
2018-02-04 00:39:08 -0500

    Added Inventory

--- src/frontend/components/inventory/dummyData.js
+++ src/frontend/components/inventory/dummyData.js
@@ -1,13 +1,13 @@
 const dummyData = [
-  {ingredientId: " ID1", ingredientName: "Salt ", temperatureZone: "frozen",
+  {inventoryId: " ID1", ingredientId: " ID1", ingredientName: "Salt ", temperatureZone: "frozen",
   quantity: "500", packageName:"Sack"},
-  {ingredientId: " ID2", ingredientName: "Pepper ", temperatureZone: "refrigerated",
+  {inventoryId: " ID2",ingredientId: " ID2", ingredientName: "Pepper ", temperatureZone: "refrigerated",
    quantity: "500", packageName:"Sack"},
-  {ingredientId: " ID3", ingredientName: "Oil ", temperatureZone: "warehouse",
+  {inventoryId: " ID3",ingredientId: " ID3", ingredientName: "Oil ", temperatureZone: "warehouse",
   quantity: "500",packageName:"Supersack"},
-  {ingredientId: " ID4", ingredientName: "Rice ", temperatureZone: "frozen",
+  {inventoryId: " ID4",ingredientId: " ID4", ingredientName: "Rice ", temperatureZone: "frozen",
   quantity: "500", packageName:"Railcar"},
-  {ingredientId: " ID5", ingredientName: "Chickpeas ", temperatureZone: "refrigerated",
+  {inventoryId: " ID5",ingredientId: " ID5", ingredientName: "Chickpeas ", temperatureZone: "refrigerated",
    quantity: "500", packageName: "Drum"},
 ];
 
--- src/frontend/components/vendors/AddVendorForm.js
+++ src/frontend/components/vendors/AddVendorForm.js
@@ -7,6 +7,13 @@ import TextField from 'material-ui/TextField';
 import {Card,CardHeader} from 'material-ui/Card';
 import * as vendorActions from '../../interface/vendorInterface.js';
 
+import { Redirect } from 'react-router'
+import * as testConfig from '../../../resources/testConfig.js'
+
+// TODO: get session Id from the user
+const sessionId = testConfig.sessionId;
+const READ_FROM_DATABASE = testConfig.READ_FROM_DATABASE;
+
 const styles = {
     buttons: {
       marginTop: 30,
@@ -17,9 +24,6 @@ const styles = {
     }
   };
 
-//TODO: get the sessionId
-const sessionId = '5a765f3d9de95bea24f905d9';
-
 class AddVendorForm extends React.Component{
   constructor(props) {
     super(props);
@@ -28,6 +32,7 @@ class AddVendorForm extends React.Component{
   		value:undefined,
       contact:'',
       code:'',
+      fireRedirect: false
       }
     // this.handleOnChange = this.handleOnChange.bind(this);
     this.onFormSubmit = this.onFormSubmit.bind(this);
@@ -42,14 +47,13 @@ class AddVendorForm extends React.Component{
     // TODO: Send data to the back end
     vendorActions.addVendor(
       this.state.name,this.state.contact,this.state.code,sessionId);
-
     e.preventDefault()
-
+    this.setState({ fireRedirect: true });
     }
 
 
   render (){
-    const { name, contact, code } = this.state;
+    const { name, contact, code, fireRedirect } = this.state;
     return (
             <div>
               <label> Add a Vendor </label>
@@ -83,13 +87,16 @@ class AddVendorForm extends React.Component{
                   <RaisedButton raised color = "secondary"
                     component = {Link} to = "/vendors">CANCEL</RaisedButton>
                   <RaisedButton raised
-                            // component = {Link} to = "/vendors"
                             color="primary"
+                            // component = {Link} to = "/vendors" //commented out because it overrides onSubmit
                             style={styles.saveButton}
                             type="Submit"
-                            primary={true}> SAVE </RaisedButton>
+                            primary="true"> SAVE </RaisedButton>
              </div>
            </form>
+           {fireRedirect && (
+             <Redirect to={'/vendors'}/>
+           )}
          </div>
     )
 	}
