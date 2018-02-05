cdbe692e867026ea2a92b77a77ad6c638adf9ad9
pratiksha sharma <pratikshasharma@pratikshas-MacBook-Pro.local>
2018-02-05 01:45:22 -0500

    Fixed bug with storage

--- src/resources/testConfig.js
+++ src/resources/testConfig.js
@@ -1,9 +1,9 @@
 //sessionId.js
 const userId = '5a765f3d9de95bea24f905d9';
-//for testing purpose only. 
+//for testing purpose only.
 const BACK_DOOR_OPEN = true; //TODO: when deploying, change to false
 const READ_FROM_DATABASE = true;
-const superId = 'real-producers-root'; 
+const superId = 'real-producers-root';
 //
 const sessionId = (BACK_DOOR_OPEN ? superId : userId);
 
