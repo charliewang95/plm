20822c9130c25de0f0afd69aaa45553a90e68c8a
Real Producers <rp@vcm-3107.vm.duke.edu>
2018-02-06 05:38:43 -0500

    save

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+'use strict';
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var config = require('./config/config'),
@@ -19,11 +20,48 @@ app.use(function(req, res, next) {
 });
 
  var fs = require('fs'),
-    https = require('https');
+    https = require('https'),
+    http = require('http');
+
+//for certificate
+/*
+const PROD = true;
+const lex = require('greenlock-express').create({
+  server: PROD ? 'https://acme-v01.api.letsencrypt.org/directory' : 'staging',
+  challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/tmp/acme-challenges' }) },
+  store: require('le-store-certbot').create({ webrootPath: '/tmp/acme-challenges' }), 
+  approveDomains: ['real-producers.colab.duke.edu', 'vcm-3107.vm.duke.edu']/*(opts, certs, cb) => {
+    if (certs) {
+      // change domain list here
+      opts.domains = ['real-producers.colab.duke.edu', 'vcm-3107.vm.duke.edu']
+    } else { 
+      // change default email to accept agreement
+      opts.email = 'ds318@duke.edu'; 
+      opts.agreeTos = true;
+    }
+    cb(null, { options: opts, certs: certs });
+  }*//*
+});
+const middlewareWrapper = lex.middleware;
+
+const redirectHttps = require('redirect-https');
+http.createServer(lex.middleware(redirectHttps())).listen(80);
+
+https.createServer(
+  lex.httpsOptions, 
+  middlewareWrapper(app)
+).listen(433);
+
+*/
+
+    http.createServer(function(req, res) {   
+        res.writeHead(301, {"Location": "https://" + req.headers['host'] + req.url});
+        res.end();
+    }).listen(80);
 
     https.createServer({
-      key: fs.readFileSync('key.pem'),
-      cert: fs.readFileSync('cert.pem')
+      key: fs.readFileSync('/etc/ssl/certs/domain.key'),
+      cert: fs.readFileSync('/etc/ssl/private/chained.pem')
     }, app).listen(config.port);
 
 //app.use(function (err, req, res, next) {
