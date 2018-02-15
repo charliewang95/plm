f6f636c2f51b9da5c9b8d2c369a96217afb94fb9
pratiksha sharma <pratikshasharma@pratikshas-mbp.home>
2018-02-15 15:28:47 -0500

    Modifying Ingredients

---
+++ src/frontend/components/ingredients/ShoppingCartButton.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import ShoppingCartIcon from 'material-ui-icons/ShoppingCart';
+import Tooltip from 'material-ui/Tooltip';
+import {Link} from 'react-router-dom';
+
+let iconStyles = {
+  fontSize: '48px'
+};
+
+const ShoppingCartButton=({ onExecute, rowId })=>{
+  return(
+    // <Tooltip title="Add to Cart">
+      // {/* <Link to = "/addVendorForm"> */}
+        <Button fab mini color="secondary" size="small" onClick={onExecute}>
+          <ShoppingCartIcon style={iconStyles}/>
+        </Button>
+      // {/* </Link> */}
+    // </Tooltip>
+  );
+};
+ShoppingCartButton.propTypes = {
+  onExecute: PropTypes.func.isRequired,
+};
+
+export default ShoppingCartButton;
