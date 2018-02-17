441bc01e32b2fa81b8c4d46faec35241467c91bc
Qianli Wang <qw42@duke.edu>
2018-02-17 14:19:24 -0500

    formula backend updated with unitsProvided

--- src/frontend/interface/formulaInterface.js
+++ src/frontend/interface/formulaInterface.js
@@ -13,11 +13,12 @@ packageType: string 'Sack', 'Pail', 'Drum', 'Supersack', 'Truckload', 'Railcar',
 temperatureZone: string 'freezer', 'refrigerator', 'warehouse', 'Freezer', 'Refrigerator', 'Warehouse'
 vendors: array of objects following the VendorPriceSchema
 **/
-function packIntoJson(name, description, ingredients){
+function packIntoJson(name, description, unitsProvided, ingredients){
 	var formulaJson = new Object();
 	formulaJson.name = name;
 	formulaJson.description = description;
 	formulaJson.ingredients = ingredients;
+	formulaJson.unitsProvided = unitsProvided;
 	console.log(ingredients);
 	return formulaJson;
 }
@@ -26,9 +27,9 @@ function packIntoJson(name, description, ingredients){
  * for arguments see packIntoJson
  * sessionId: string, id of the current session
  */
-async function addFormula(name, description, ingredients, sessionId, callback) {
+async function addFormula(name, description, unitsProvided, ingredients, sessionId, callback) {
 	console.log("add formulas");
-	var newFormula = packIntoJson(name, description, ingredients);
+	var newFormula = packIntoJson(name, description, unitsProvided, ingredients);
 	//return await formulaActions.addFormula(newFormula, sessionId);
 	formulaActions.addFormula(newFormula, sessionId, function(res){
 	    callback(res);
@@ -78,8 +79,8 @@ async function getFormulaAsync(formulaId, sessionId) {
  * other arguments: see packIntoJson()
  * sessionId: string, id of the current session
  */
-async function updateFormula(formulaId, name, description, ingredients, sessionId, callback) {
-	var updatedFormula = packIntoJson(name, description, ingredients);
+async function updateFormula(formulaId, name, description, unitsProvided, ingredients, sessionId, callback) {
+	var updatedFormula = packIntoJson(name, description, unitsProvided, ingredients);
 	//return await formulaActions.updateFormula(formulaId, sessionId, updatedFormula);
 	formulaActions.updateFormula(formulaId, sessionId, updatedFormula, function(res){
 	    callback(res);
