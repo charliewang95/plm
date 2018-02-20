f0d427e7b4e4e33fa6675120d1d5715296cbd233
Dan Sun <ds318@duke.edu>
2018-02-20 14:27:58 -0500

    changed so that rows reflect state, also added column filters

--- src/frontend/components/admin/users/GridExample.js
+++ src/frontend/components/admin/users/GridExample.js
@@ -3,30 +3,68 @@ import React from 'react';
 import { render } from 'react-dom';
 import Paper from 'material-ui/Paper';
 import {
+  FilteringState,
+  IntegratedFiltering,
+} from '@devexpress/dx-react-grid'; //for filtering
+import {
   // State or Local Processing Plugins
 } from '@devexpress/dx-react-grid';
 import {
   Grid,
   Table,
-  TableHeaderRow
+  TableHeaderRow,
+  TableFilterRow, // for filtering
 } from '@devexpress/dx-react-grid-material-ui';
 
+export default class SampleTable extends React.PureComponent {
+	constructor(props) {
+		super(props);
 
-const App = () => (
-  <Grid
-    rows={[
-      { username: 'sampleUser', email: 'user@duke.edu', privelege: 'user' },
-      { username: 'sampleManager', email: 'manager@duke.edu', privelege: 'manager' },
-      { username: 'sampleAdmin', email: 'admin@duke.edu', privelege: 'admin'}
-    ]}
-    columns={[
-      { name: 'username', title: 'Username' },
-      { name: 'email', title: 'E-mail' },
-      { name: 'privelege', title: 'Privelege' },
-    ]}>
-    <Table />
-    <TableHeaderRow />
-  </Grid>
-);
+		this.state = {
+			columns: [
+      			{ name: 'username', title: 'Username' },
+      			{ name: 'email', title: 'E-mail' },
+      			{ name: 'privelege', title: 'Privelege' },
+      		],
+      		rows: [
+      			{ username: 'sampleUser', email: 'user@duke.edu', privelege: 'user' },
+     			{ username: 'sampleManager', email: 'manager@duke.edu', privelege: 'manager' },
+      			{ username: 'sampleAdmin', email: 'admin@duke.edu', privelege: 'admin'},
+      		]
+      	}
+	}
 
-export default App;
+	render() {
+		const { rows, columns } = this.state;
+		return(
+			<Grid
+				rows={rows}
+				columns={columns}
+			>
+				<FilteringState defaultFilters={[]} />
+				<IntegratedFiltering />
+				<Table />
+				<TableHeaderRow />
+				<TableFilterRow />
+			</Grid>
+		);
+	}
+}
+// const App = () => (
+//   <Grid
+//     rows={[
+//       { username: 'sampleUser', email: 'user@duke.edu', privelege: 'user' },
+//       { username: 'sampleManager', email: 'manager@duke.edu', privelege: 'manager' },
+//       { username: 'sampleAdmin', email: 'admin@duke.edu', privelege: 'admin'}
+//     ]}
+//     columns={[
+//       { name: 'username', title: 'Username' },
+//       { name: 'email', title: 'E-mail' },
+//       { name: 'privelege', title: 'Privelege' },
+//     ]}>
+//     <Table />
+//     <TableHeaderRow />
+//   </Grid>
+// );
+
+// export default App;
