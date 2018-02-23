7a7bac2e21a4d36cc0512900b65535d292fdfbf2
Dan Sun <ds318@duke.edu>
2018-02-23 17:35:23 -0500

    added editing, deleting, and new buttons

--- src/frontend/components/admin/users/GridExample.js
+++ src/frontend/components/admin/users/GridExample.js
@@ -11,11 +11,18 @@ import {
 	IntegratedSorting
 } from '@devexpress/dx-react-grid'; // for sorting
 import {
+	EditingState,
+} from '@devexpress/dx-react-grid'; // for editing
+import {
   Grid,
   Table,
   TableHeaderRow,
   TableFilterRow, // for filtering
+  TableEditRow, //for editing
+  TableEditColumn, //for editing
 } from '@devexpress/dx-react-grid-material-ui';
+
+const getRowId = row => row.id;
 
 export default class SampleTable extends React.PureComponent {
 	constructor(props) {
@@ -23,27 +30,84 @@ export default class SampleTable extends React.PureComponent {
 
 		this.state = {
 			columns: [
-      			{ name: 'username', title: 'Username' },
-      			{ name: 'email', title: 'E-mail' },
-      			{ name: 'privelege', title: 'Privelege' },
+      			{ 
+      				name: 'username', 
+      				title: 'Username',
+      				getCellValue: row => (row.username ? row.username : undefined), 
+      			},
+      			{ 
+      				name: 'email', 
+      				title: 'E-mail',
+      				getCellValue: row => (row.email ? row.email : undefined), 
+      			},
+      			{ 
+      				name: 'privelege', 
+      				title: 'Privelege',
+      				getCellValue: row => (row.privelege ? row.privelege : undefined), 
+      			},
       		],
+
       		rows: [
-      			{ username: 'sampleUser', email: 'user@duke.edu', privelege: 'user' },
-     			{ username: 'sampleManager', email: 'manager@duke.edu', privelege: 'manager' },
-      			{ username: 'sampleAdmin', email: 'admin@duke.edu', privelege: 'admin'},
+      			{ id: 0, username: 'sampleUser', email: 'user@duke.edu', privelege: 'user' }, //id for editing
+     			{ id: 1, username: 'sampleManager', email: 'manager@duke.edu', privelege: 'manager' },
+      			{ id: 2, username: 'sampleAdmin', email: 'admin@duke.edu', privelege: 'admin'},
       		],
+
       		sorting: [{ columnName: 'username', direction: 'asc' }],
+
+      		editingColumnExtensions: [
+        		{
+         			columnName: 'username',
+          			createRowChange: (row, value) => ( {...row, username: value} ),
+        		},
+        		{
+          			columnName: 'email',
+          			createRowChange: (row, value) => ( {...row, email: value} ),
+       			},
+        		{
+          			columnName: 'privelege',
+          			createRowChange: (row, value) => ( {...row, privelege: value} ),
+        		},
+      		],
       	}
 
-      	this.changeSorting = sorting => this.setState( {sorting} );
+      	this.changeSorting = sorting => this.setState( {sorting} );// for sorting
+      	this.commitChanges = this.commitChanges.bind(this); // for editing
 	}
 
+	commitChanges({ added, changed, deleted }) { //function for saving changes in table
+    let { rows } = this.state;
+    console.log("Entered commitChanges()");
+    if (added) {
+      console.log("entering added");
+      const startingAddedId = (rows.length - 1) > 0 ? rows[rows.length - 1].id + 1 : 0;
+      rows = [
+        ...rows,
+        ...added.map((row, index) => ({
+          id: startingAddedId + index,
+          ...row,
+        })),
+      ];
+    }
+    if (changed) {
+    	console.log("entering changed");
+      rows = rows.map(row => (changed[row.id] ? { ...row, ...changed[row.id] } : row));
+    }
+    if (deleted) {
+    	console.log("entering deleted")
+      const deletedSet = new Set(deleted);
+      rows = rows.filter(row => !deletedSet.has(row.id));
+    }
+    this.setState({ rows });
+  }
+
 	render() {
-		const { rows, columns, sorting } = this.state;
+		const { rows, columns, sorting, editingColumnExtensions } = this.state;
 		return(
 			<Grid
 				rows={rows}
 				columns={columns}
+				getRowId={getRowId}
 			>
 				<FilteringState defaultFilters={[]} />
 				<IntegratedFiltering />
@@ -52,9 +116,15 @@ export default class SampleTable extends React.PureComponent {
 					onSortingChange={this.changeSorting}
 				/>
 				<IntegratedSorting />
+				<EditingState
+            		columnExtensions={editingColumnExtensions}
+            		onCommitChanges={this.commitChanges}
+          		/>
 				<Table />
 				<TableHeaderRow showSortingControls />
-				<TableFilterRow />
+				<TableFilterRow /> 
+				<TableEditRow />
+          		<TableEditColumn showAddCommand showEditCommand showDeleteCommand />
 			</Grid>
 		);
 	}
