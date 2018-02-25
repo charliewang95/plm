b366bfcec329b82079135b7c6de2026aea0c31ba
Dan Sun <ds318@duke.edu>
2018-02-25 12:19:35 -0500

    added radio buttons in the front end

--- src/frontend/components/admin/users/RadioButtonExample.js
+++ src/frontend/components/admin/users/RadioButtonExample.js
@@ -6,23 +6,22 @@ import Radio, { RadioGroup } from 'material-ui/Radio';
 import { FormLabel, FormControl, FormControlLabel, FormHelperText } from 'material-ui/Form';
 
 const styles = theme => ({
-  root: {
-    display: 'flex',
-  },
   formControl: {
-    margin: theme.spacing.unit * 3,
+    marginTop: theme.spacing.unit * 3,
   },
   group: {
     margin: `${theme.spacing.unit}px 0`,
+    display : 'flex',
   },
 });
 
 class RadioButtonsGroup extends React.Component {
   state = {
-    value: 'female',
+    value: 'user',
   };
 
   handleChange = (event, value) => {
+  	console.log("changing value to " + value);
     this.setState({ value });
   };
 
@@ -30,49 +29,21 @@ class RadioButtonsGroup extends React.Component {
     const { classes } = this.props;
 
     return (
-      <div className={classes.root}>
         <FormControl component="fieldset" required className={classes.formControl}>
-          <FormLabel component="legend">Gender</FormLabel>
+          <FormLabel component="legend">Privilege</FormLabel>
           <RadioGroup
-            aria-label="gender"
-            name="gender1"
+            aria-label="privilege"
+            name="privilege"
             className={classes.group}
             value={this.state.value}
             onChange={this.handleChange}
+            row={true}
           >
-            <FormControlLabel value="female" control={<Radio />} label="Female" />
-            <FormControlLabel value="male" control={<Radio />} label="Male" />
-            <FormControlLabel value="other" control={<Radio />} label="Other" />
-            <FormControlLabel
-              value="disabled"
-              disabled
-              control={<Radio />}
-              label="(Disabled option)"
-            />
+            <FormControlLabel value="user" control={<Radio />} label="User" />
+            <FormControlLabel value="manager" control={<Radio />} label="Manager" />
+            <FormControlLabel value="admin" control={<Radio />} label="Admin" />
           </RadioGroup>
         </FormControl>
-        <FormControl component="fieldset" required error className={classes.formControl}>
-          <FormLabel component="legend">Gender</FormLabel>
-          <RadioGroup
-            aria-label="gender"
-            name="gender2"
-            className={classes.group}
-            value={this.state.value}
-            onChange={this.handleChange}
-          >
-            <FormControlLabel value="male" control={<Radio color="primary" />} label="Male" />
-            <FormControlLabel value="female" control={<Radio color="primary" />} label="Female" />
-            <FormControlLabel value="other" control={<Radio color="primary" />} label="Other" />
-            <FormControlLabel
-              value="disabled"
-              disabled
-              control={<Radio />}
-              label="(Disabled option)"
-            />
-          </RadioGroup>
-          <FormHelperText>You can display an error</FormHelperText>
-        </FormControl>
-      </div>
     );
   }
 }
