6179f0eed1ac7be4908ecee13c40f743b7f1bf76
Alexander Tseng <xanderyst@gmail.com>
2018-04-15 17:13:01 -0400

    fixed editing production line

--- src/frontend/components/productionLine/ProductionLine.js
+++ src/frontend/components/productionLine/ProductionLine.js
@@ -180,7 +180,6 @@ class ProductionLine extends React.PureComponent {
     this.changeRowChanges = (rowChanges) => this.setState({ rowChanges });
     this.changeCurrentPage = currentPage => this.setState({ currentPage });
     this.changePageSize = pageSize => this.setState({ pageSize });
-
     this.commitChanges = ({ deleted }) => {
       let { rows } = this.state;
       this.setState({ rows, deletingRows: deleted || this.state.deletingRows });
@@ -255,7 +254,7 @@ class ProductionLine extends React.PureComponent {
             formulaNamesString+=', ';
           }
         }
-        rawData[i].formulaNames = formulaNamesString; 
+        rawData[i].formulaNamesString = formulaNamesString; 
         rawData[i].isIdle = rawData[i].isIdle ? "Idle" : "Busy";
         rawData[i].productionLineId = rawData[i]._id;
       }
